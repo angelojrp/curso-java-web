9a9defc41fb69494948a347c4a0269e8fcd79f8f
aluno.4rlab02 <aluno.4rlab02@4RLAB02-11.acadnet.aeudf.br>
2015-11-07 13:07:22 -0200

    a2

--- src/main/java/br/com/tutorial/model/CadastroDTO.java
+++ src/main/java/br/com/tutorial/model/CadastroDTO.java
@@ -10,14 +10,49 @@ public class CadastroDTO implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private Date data;
+	private String nome, desc, estado, categoria;
+	private int tipo;
 
-	public Date getData() {
-		return data;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setData(Date data) {
-		this.data = data;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
 
 }
